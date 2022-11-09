1fc6be891b6895cf661791fc58322f9787d369a9
cph-mtnl <mtnl@cphbusiness.dk>
2022-11-09 10:25:50 +0100

    added the Modal

--- src/components/shared/TheHeader.jsx
+++ src/components/shared/TheHeader.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import styles from "./TheHeader.module.css";
+import Modal from "react-modal";
 
 export default function TheHeader() {
+  let [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <header className={styles.header}>
       <h1>Business card</h1>
-      <button className={styles.button}>Create new</button>
+      <button onClick={handleClick} className={styles.button}>
+        Create new
+      </button>
+      <Modal
+        isOpen={showModal}
+        onRequestClose={closeModal}
+        contentLabel="Create new business card"
+      >
+        <h1>Temporary</h1>
+      </Modal>
     </header>
   );
 }
