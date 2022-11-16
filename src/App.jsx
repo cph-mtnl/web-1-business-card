5aa5602f0f8ba13bdf29472a00d335a79a2d83ad
cph-mtnl <mtnl@cphbusiness.dk>
2022-11-16 11:32:05 +0100

    cleaned up a bit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import MainPage from "./pages/MainPage.jsx";
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -20,25 +20,3 @@ function App() {
 }
 
 export default App;
-
-/*
-
-reference:
-
-    <div>
-      <nav>
-        {buttons.map((button) => (
-          <Link key={button.buttonText} to={button.route}>
-            {button.buttonText}
-          </Link>
-        ))}
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home title="Home" />} exact />
-        <Route path="/about" element={<About title="About" />} />
-        <Route path="/contact" element={<Contact title="Contact" />} />
-        <Route element={<Page404 />} />
-      </Routes>
-    </div>
-
-*/
