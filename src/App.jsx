f13d360d2df258220c980386876b8f61d0f2cf8a
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2022-11-23 08:27:31 +0100

    preparing for lecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import MainPage from "./pages/MainPage.jsx";
 import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<MainPage />} exact />
-        <Route
-          path="/login"
-          element={
-            <div>
-              <h1>Please log in</h1>
-            </div>
-          }
-        />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </>
   );
